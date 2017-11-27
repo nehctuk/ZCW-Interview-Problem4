201690ff37dcfb3730e7b0418ef3f587de46c5a6
nehctuk <nehctuk@gmail.com>
2017-11-27 17:13:35 -0500

    Done

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,58 @@
 package io.zipcoder;
 
+import java.util.*;
+
 public class Problem4 {
+
+    public String isPalindromePermutation(String input) {
+        if (countOfOddLetters(input) == 0) {
+            return isPalindromePermutationEvenLength(input);
+        }
+        else {
+            return isPalindromePermutationOddLength(input);
+        }
+    }
+
+    public String isPalindromePermutationEvenLength(String input) {
+        if (input.length()%2==0) {
+            Integer countOfOddLetters = countOfOddLetters(input);
+            if (countOfOddLetters==0) {
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    public String isPalindromePermutationOddLength(String input) {
+        if (!(input.length()%2==0)) {
+            Integer countOfOddLetters = countOfOddLetters(input);
+            if (countOfOddLetters==1) {
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    public Integer countOfOddLetters(String input) {
+        Integer countOfOddLetters = 0;
+        for (Integer aLetterCount : mapOfLetters(input).values()) {
+            if (!(aLetterCount%2==0)) {
+                countOfOddLetters++;
+            }
+        }
+        return countOfOddLetters;
+    }
+
+    public Map<String, Integer> mapOfLetters(String input) {
+        Map<String, Integer> frequencyMap = new HashMap<>();
+        for (String aLetter :input.split("")) {
+            if (frequencyMap.containsKey(aLetter)) {
+                frequencyMap.put(aLetter, frequencyMap.get(aLetter)+1);
+            }
+            else {
+                frequencyMap.put(aLetter, 1);
+            }
+        }
+        return frequencyMap;
+    }
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,64 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem4Test {
+    Problem4 problem4 = new Problem4();
+    String input = "aabcbaa";
+
+    @Test
+    public void mapOfLettersTest() {
+        Integer expected = 4;
+        Integer actual = problem4.mapOfLetters(input).get("a");
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isPalindromePermutationTest() {
+        String expected = "YES";
+        String actual = problem4.isPalindromePermutationOddLength(input);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void isPalindromePermutationOddTest_aaabbbb() {
+        String input = "aaabbbb";
+        String expected = "YES";
+        String actual = problem4.isPalindromePermutationOddLength(input);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void isPalindromePermutationTest_cdefghmnopqrstuvw() {
+        String input = "cdefghmnopqrstuvw";
+        String expected = "NO";
+        String actual = problem4.isPalindromePermutation(input);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void isPalindromePermutationTest_cdcdcdcdeeeef() {
+        String input = "cdcdcdcdeeeef";
+        String expected = "YES";
+        String actual = problem4.isPalindromePermutation(input);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void isPalindromePermutationEvenLengthTest() {
+        String input = "aabbbbaa";
+        String expected = "YES";
+        String actual = problem4.isPalindromePermutationEvenLength(input);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void countOfOddLettersTest() {
+        String input = "cdcdcdcdeeeef";
+        Integer expected = 1;
+        Integer actual = problem4.countOfOddLetters(input);
+        Assert.assertEquals(expected,actual);
+    }
 }
